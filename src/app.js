db55c94b82b24da19952d2a1f4e9694c0acad18b
DobleAC <votaredes@gmail.com>
2024-12-04 19:23:27 -0700

    Corrección en archivos sobre el puerto 3000 por el 3001

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const tiposEnvioRoutes = require('./routes/tipoEnvioRoutes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware para parsear el cuerpo de las peticiones
 app.use(express.json());
